6ae8eae5fe2c18251150745db10082515c1998c6
dread <dread@DESKTOP-BS1AKPM>
2020-09-20 02:15:57 -0700

    Add project files.

---
+++ Homework3/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿/// File Name: Homework 3
+/// @author: Dakota Durst
+/// Date:  September 19, 2020
+///
+/// Problem Statement: Take user input of total mortgage and mortgage payment and print out amount paid towards monthly interest and towards total mortgage
+/// 
+/// 
+/// 
+/// Overall Plan:
+/// 1) assign mortgage and payment equal to user input (type cast string to double)
+/// 2) assign other variables such as interest, princple and others needed for math
+/// 3) get payment and subtract monthy interest (assume value is greater then zero)
+/// 4) take result and output both monthly interst cost and amount paid towards mortgage
+/// 4) set mortgage to new value and output new total mortgage
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string balance = total_balance.Text; //user outstanding balance
+            double balance1 = Convert.ToDouble(balance); //convert string to double
+            string payment = user_payment.Text; //user monthy payment
+            double payment1 = Convert.ToDouble(payment); //convert string to double
+            double flat_interest = .0639; //percentage of cost added to balance per year
+            double month_interest = flat_interest / 12; //percent of cost increase of balance per month
+            double interest_payment = month_interest * balance1; //amount of money added to balance per month
+            double principle = payment1 - interest_payment; //money that will be subtracted to outstanding balance after interest_payment is paid off (assumes value is positive)
+            Console.WriteLine(balance1);
+            balance1 -= principle; //sets balance to new value based on money paid off
+            Console.WriteLine(balance1);
+
+            MessageBox.Show("Your payment of: " + payment1 + "$ has paid off: " + interest_payment + "$ of monthy interest and: " + principle + 
+                "$ off your total mortgage leaving you with: " + balance1 + "$ remaining on your mortgage"); 
+  
+
+
+
+
+
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,53 @@
+﻿/// File Name: Homework 3 part 2
+/// @author: Dakota Durst
+/// Date:  September 19, 2020
+///
+/// Problem Statement: Take user input of item price (bewtween .25 and 1.00 in increments of 5) and calculate change based on user giving 1 dollar to buy item
+///
+/// 
+/// Overall Plan:
+/// 1) assign price to user input (type cast from string to int)
+/// 2) assign other variables such as quarters, dimes, nickles, and change after each respective coin 
+/// 3) subtract 100 by price to get change
+/// 4) take result and divide by 25, edit change to amount of times 25 goes into change
+/// 5) repeat for dimes and nickles
+/// 6) print out amount of quarters, dimes, and nickles given to customer
+
+
+using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            Console.WriteLine("Enter price of item (from 25 cents to a dollar, in 5-cent increments");
+            int price = Convert.ToInt32(Console.ReadLine());
+            int change = 100 - price;
+            int num_quarter = 0;
+            int num_dime = 0;
+            int num_nickle = 0;
+            int change_afterQ = 0;
+            int change_afterD = 0;
+            int change_afterN = 0;
+
+            ///Processing
+            
+            num_quarter = change / 25;
+            //calculate remaining change
+            change_afterQ = change % 25;
+            num_dime = change_afterQ / 10;
+            change_afterD = change_afterQ % 10;
+            num_nickle = change_afterD / 5;
+            change_afterN = change_afterD % 5;
+
+            ///output
+            Console.WriteLine("You bought an item for " + price + " cents and gave me a dollar, so your change is:");
+            Console.WriteLine(num_quarter + " quarters");
+            Console.WriteLine(num_dime + " dimes and");
+            Console.WriteLine(num_nickle + " nickles.");
+        }
+    }
+}
